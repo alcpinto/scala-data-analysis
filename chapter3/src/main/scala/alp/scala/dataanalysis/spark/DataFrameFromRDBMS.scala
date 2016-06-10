b525cf23f0c43cc76f1dce8893a2096f8134d242
alpinto <Abilio.Pinto@i2s.pt>
2016-06-10 18:14:42 +0100

    Load data from RDBMS into DataFrames

---
+++ chapter3/src/main/scala/alp/scala/dataanalysis/spark/DataFrameFromRDBMS.scala
@@ -0,0 +1,32 @@
+package alp.scala.dataanalysis.spark
+
+/**
+  * Created by ALPinto on 10/06/2016.
+  */
+object DataFrameFromRDBMS extends App {
+
+  import com.typesafe.config.ConfigFactory
+  import org.apache.spark.sql.SQLContext
+  import org.apache.spark.{SparkConf, SparkContext}
+
+  val conf = new SparkConf().setAppName("DataFromRDBMS").setMaster("local[2]")
+  val sc = new SparkContext(conf)
+  val sqlContext = new SQLContext(sc)
+
+
+  val config = ConfigFactory.load()
+  val options = Map(
+    "driver" -> config.getString("hsqldb.driver"),
+    "url" -> config.getString("hsqldb.connection.url"),
+    "dbtable" -> "(select * from PUBLIC.student) student",
+    "partitionColumn" -> "id",
+    "lowerBound" -> "1",
+    "upperBound" -> "100",
+    "numPartitions"-> "2")
+
+  val dFrame = sqlContext.read.options(options).format("jdbc").load
+  dFrame.printSchema()
+  dFrame.show()
+
+
+}
